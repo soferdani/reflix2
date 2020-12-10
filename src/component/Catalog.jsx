914a2a870af14841b2b1f859962286a9383c7d56
soferdani <soferdani@gmail.com>
2020-12-10 12:41:46 +0200

    add some stilys

--- src/component/Catalog.jsx
+++ src/component/Catalog.jsx
@@ -1,13 +1,22 @@
 import Movie from "./Movie";
+import '../styles/Catalog.css'
 
 function Catalog(props) {
+    console.log(props.state.users[0].budget);
+
+
+
     return (
         <div className="catalog">
             <div className="catalog-header">
                 <input className='search' placeholder='search' />
-                <span className='budget'>{ props.state.budget}</span>
+                <div className="userInfo">
+                    <img id='userInfoAvatar' src="" alt="" />
+                    <h3>{props.state.users[0].name},</h3>
+                    <h3>Budget: {props.state.users[0].budget} .00$</h3>
+                </div>
             </div>
-            <div className="catalogInfo">
+            <div className="movieContainer">
                 {props.state.catalog.map(m => {
                     return (
                         <Movie id={m.id} img={m.img} title={m.title} key={m.id} rented={m.isRented}/>
